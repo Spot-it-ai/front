02547faf33eac73605e4ebeeade8ee46d80be768
Sidharth Malhotra <sidharthmalhotra0094@gmail.com>
2020-06-13 14:17:58 -0400

    [TASK] Added search component and dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardModule } from './dashboard/dashboard.module';
 
 @NgModule({
   declarations: [
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,13 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { SearchComponent } from './components/search/search.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 
 @NgModule({
-  declarations: [SearchComponent],
+  declarations: [SearchComponent, DashboardComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule,
+    FlexLayoutModule,
+
+    // material ui
+    MatInputModule,
+    MatButtonModule,
   ],
 })
 export class DashboardModule { }
